5064c2900748af0ede3570a01018e7682e71eee4
Gary Pang <gary.pang@gmail.com>
2019-03-01 23:35:28 -0500

    Refactor seed script to use Node cluster
    Generate data with "worker" processes that run multiple for-loops.
    
    Seed script can now generate 4M records at about 21912.358ms.

--- server/db/mongodb/seed1.js
+++ server/db/mongodb/seed1.js
@@ -1,37 +1,46 @@
+const cluster = require('cluster');
 const path = require('path');
 const now = require('performance-now');
- 
-let makeReview = function () {
-  let newReview = {
-    reviewCount: 0,
-    stars: 0
+
+if (cluster.isMaster) {
+  // Count the machine's CPUs
+  let cpuCount = require('os').cpus().length;
+
+  // Create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+      cluster.fork();
+  }  
+} else {
+  let makeReview = function () {
+    let newReview = {
+      reviewCount: 0,
+      stars: 0
+    }
+    return newReview;
   }
-  return newReview;
+
+  let MongoClient = require('mongodb').MongoClient;
+  MongoClient.connect('mongodb://localhost:27017/amazon-cart', (err, db) => {
+    if (err) throw err;
+    console.log('Connected To DB');
+    const dbo = db.db('amazon-cart');
+
+    let documents = [];
+
+    // track starting time for seeding
+    let startTime01 = now();
+
+    for (let records = 0; records < 1000000; records++) {
+      documents.push(makeReview());  
+    }
+
+    dbo.collection("reviews").insertMany(documents, function (err, docs) {
+        if (err) throw err;
+        console.log(docs.result);
+        console.log(`Database seeding took ${now() - startTime01}ms`);
+        console.log(process.memoryUsage());
+        db.close();
+    });
+
+  });  
 }
-
-let MongoClient = require('mongodb').MongoClient;
-MongoClient.connect('mongodb://localhost:27017/amazon-cart', (err, db) => {
-  if (err) throw err;
-  console.log('Connected To DB');
-  const dbo = db.db('amazon-cart');
-
-  let documents = [];
-
-  // track starting time for seeding
-  let startTime01 = now();
-
-  for (let records = 0; records < 1000000; records++) {
-    documents.push(makeReview());  
-  }
-
-  dbo.collection("reviews").insertMany(documents, function (err, docs) {
-      if (err) throw err;
-      console.log(docs.result);
-      
-      console.log(`Database seeding took ${now() - startTime01}ms`);
-      console.log(`${docs.length} documents saved to database`);
-      // console.log(process.memoryUsage());
-      db.close();
-  });
-
-});
