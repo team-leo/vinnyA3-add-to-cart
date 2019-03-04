82b0966ad043487c7cebf1a3fa521adda1032b7f
Gary Pang <gary.pang@gmail.com>
2019-03-04 15:19:21 -0500

    Refactor Review seed script (Postgres)
    Script generated 10 million DB records at 205911.013554ms.
    
    TODOS:
    Optimize Review script with Node cluster workers.
    Write Product seeding script.

--- server/db/postgres/seedReview.js
+++ server/db/postgres/seedReview.js
@@ -1,34 +1,34 @@
-// const pg = require('pg');
 const { Pool, Client } = require('pg');
+const pgp = require('pg-promise')({capSQL: true});
 const path = require('path');
 const now = require('performance-now');
 const config = require(path.join(__dirname, '../../../server/config/postgres/config'));
 const genRandomInt = require(path.join(__dirname, '../../../server/utils/genRandInt'));
 
-let pool = new Pool(config);
+// let pool = new Pool(config);
+const db = pgp(config);
 
-pool.query('CREATE TABLE reviews(id SERIAL PRIMARY KEY, reviewCount INTEGER, stars INTEGER DEFAULT 0)', (err, res) => {
-    console.log(err ? err.stack : res);
-    console.log(`${now()}ms`);
-});
+const cs = new pgp.helpers.ColumnSet([
+    {name: 'reviewcount', def: 0},
+    {name: 'stars', def: 0}
+], {table: 'reviews'});
 
-// const client = new Client(config);
-// client.connect();
 
 let startTime = now();
+let data = [];
 
-for (let index = 0; index < 10000; index++) {
-    pool.query('INSERT INTO reviews(reviewCount, stars) VALUES($1, $2)', [genRandomInt(0, 801), genRandomInt(0, 5)], (err, results) => {
-        if (err) {
-            console.log(err);
-            console.log(`${now()}ms`);
-        } else {
-            console.log(results.rows);
-        }
-    });
+for (let index = 0; index < 10000000; index++) {
+    data.push({reviewcount: genRandomInt(0, 801), stars: genRandomInt(0, 5)});
 }
 
-pool.end(() => {
-  console.log('pool has ended');
-  console.log(`${now() - startTime}ms`);
+db.task('inserting-reviews', t => {
+    const insert = pgp.helpers.insert(data, cs);
+    return t.none(insert);
 })
+.then(() => {
+    console.log(`Success: Database seeding took ${now() - startTime}ms`);
+})
+.catch(error => {
+    console.log(error);
+    console.log(`Operation time: ${now() - startTime}ms`);
+});
