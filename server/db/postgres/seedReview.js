ed072824833b4ba6ad7f2b980c456c83286d5af8
Gary Pang <gary.pang@gmail.com>
2019-03-04 14:05:07 -0500

    Install npm module pg-promise to use Postgres promise

--- server/db/postgres/seedReview.js
+++ server/db/postgres/seedReview.js
@@ -1,33 +1,34 @@
-const pg = require('pg');
+// const pg = require('pg');
+const { Pool, Client } = require('pg');
 const path = require('path');
 const now = require('performance-now');
+const config = require(path.join(__dirname, '../../../server/config/postgres/config'));
 const genRandomInt = require(path.join(__dirname, '../../../server/utils/genRandInt'));
 
-// const client = new Client();
-
-let config = {
-    host: 'localhost',
-    user: 'postgres',
-    password: '*******', // TODO: Replace with environmental variable
-    database: 'amazoncart'
-};
-
-let pool = new pg.Pool(config);
-
-// client.connect();
-
+let pool = new Pool(config);
 
 pool.query('CREATE TABLE reviews(id SERIAL PRIMARY KEY, reviewCount INTEGER, stars INTEGER DEFAULT 0)', (err, res) => {
     console.log(err ? err.stack : res);
-    // client.end();
+    console.log(`${now()}ms`);
 });
 
-for (let index = 0; index < 1000000; index++) {
+// const client = new Client(config);
+// client.connect();
+
+let startTime = now();
+
+for (let index = 0; index < 10000; index++) {
     pool.query('INSERT INTO reviews(reviewCount, stars) VALUES($1, $2)', [genRandomInt(0, 801), genRandomInt(0, 5)], (err, results) => {
         if (err) {
             console.log(err);
+            console.log(`${now()}ms`);
         } else {
-            console.log(results);
+            console.log(results.rows);
         }
-    });    
+    });
 }
+
+pool.end(() => {
+  console.log('pool has ended');
+  console.log(`${now() - startTime}ms`);
+})
