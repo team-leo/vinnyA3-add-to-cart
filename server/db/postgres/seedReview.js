e98def8df0214df03625e12ff37e0c1383c9366d
Gary Pang <gary.pang@gmail.com>
2019-03-04 16:03:43 -0500

    Use Node cluster in Postgres seed script

--- server/db/postgres/seedReview.js
+++ server/db/postgres/seedReview.js
@@ -1,34 +1,51 @@
 const { Pool, Client } = require('pg');
 const pgp = require('pg-promise')({capSQL: true});
+const cluster = require('cluster');
 const path = require('path');
 const now = require('performance-now');
 const config = require(path.join(__dirname, '../../../server/config/postgres/config'));
 const genRandomInt = require(path.join(__dirname, '../../../server/utils/genRandInt'));
 
-// let pool = new Pool(config);
-const db = pgp(config);
 
-const cs = new pgp.helpers.ColumnSet([
+if (cluster.isMaster) {
+  // Count the machine's CPUs
+  let cpuCount = require('os').cpus().length;
+
+  // Create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+      cluster.fork();
+  }
+  // If worker dies, replace with new worker
+  cluster.on('exit', function (worker) {
+    console.log('Worker %d has died', worker.id);
+    cluster.fork();
+  });
+
+} else {
+  let db = pgp(config);
+  
+  let cs = new pgp.helpers.ColumnSet([
     {name: 'reviewcount', def: 0},
     {name: 'stars', def: 0}
-], {table: 'reviews'});
+  ], {table: 'reviews'});
 
-
-let startTime = now();
-let data = [];
-
-for (let index = 0; index < 10000000; index++) {
+  let startTime = now();
+  let data = [];
+  
+  for (let index = 0; index < 2500000; index++) {
     data.push({reviewcount: genRandomInt(0, 801), stars: genRandomInt(0, 5)});
-}
-
-db.task('inserting-reviews', t => {
+  }
+  
+  db.task('inserting-reviews', t => {
     const insert = pgp.helpers.insert(data, cs);
     return t.none(insert);
-})
-.then(() => {
+  })
+  .then(() => {
     console.log(`Success: Database seeding took ${now() - startTime}ms`);
-})
-.catch(error => {
+    console.log(`${data.length} records generated`);
+  })
+  .catch(error => {
     console.log(error);
     console.log(`Operation time: ${now() - startTime}ms`);
-});
+  });    
+}
